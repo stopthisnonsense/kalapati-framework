13ad292cd0c3c01a0189bc549ccd2096f88609f0
Amarvin <anthony@glacial.com>
2021-03-09 13:45:00 -0500

    Get canvas Started

---
+++ src/js/bundle.js
@@ -0,0 +1,26 @@
+const gameArea = () => {
+  myGameArea.canvas = document.querySelector('.game-area');
+  myGameArea.start();
+    // console.log( myGameArea.context )
+  let playerPiece = new component(50, 50, 'blue', 10, 10);
+};
+
+const myGameArea = {
+  canvas: '',
+  start: function () {
+    this.context = this.canvas.getContext('2d');
+  },
+  clear: ( ) => {
+
+  }
+};
+function component(width, height, color, x, y) {
+  this.width = width;
+  this.height = height;
+  this.x = x;
+  this.y = y;
+  this.ctx = myGameArea.context;
+  this.ctx.fillStyle = color;
+  this.ctx.fillRect(this.x, this.y, this.width, this.height);
+};
+gameArea();
