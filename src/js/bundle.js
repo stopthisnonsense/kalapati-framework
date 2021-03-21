f6af46fe5bd041a91d3fe6debd288b438c10d39a
Amarvin <anthony@glacial.com>
2021-03-21 15:41:58 -0400

    code cleanup

--- src/js/bundle.js
+++ src/js/bundle.js
@@ -18,9 +18,7 @@ const load = () => {
 			}
 		}
 	};
-
-	clickerButton.addEventListener('click', () => {
-		numbers.clicks += 1;
+	const numberCount = () => {
 		numbers.x = round(
 			numbers.clicks + 0.01 * round(numbers.y * (0.01 * numbers.z))
 		);
@@ -32,28 +30,22 @@ const load = () => {
 			numbers.z += 1;
 			console.log(`number Z is: ${numbers.z}`);
 		}
+	};
+	clickerButton.addEventListener('click', () => {
+		numbers.clicks += 1;
+		numberCount();
 		console.log(numbers.x);
 		infoUpdate();
 		// alert('it work!');
 	});
 	setInterval(() => {
 		numbers.clicks++;
-		numbers.x = round(
-			numbers.clicks + 0.01 * round(numbers.y * (0.01 * numbers.z))
-		);
-		if (numbers.clicks % 10 == 0) {
-			numbers.y += 1;
-			console.log(`number Y is: ${numbers.y}`);
-		}
-		if (numbers.y % 10 == 0 && numbers.clicks % 10 == 0) {
-			numbers.z += 1;
-			console.log(`number Z is: ${numbers.z}`);
-		}
+		numberCount();
 		infoUpdate();
 	}, 1000);
 
-	function round(value, decimals = 2) {
+	const round = (value, decimals = 2) => {
 		return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals);
-	}
+	};
 };
 load();
