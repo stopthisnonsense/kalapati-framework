3fd30dbabf2b5c7e8ec0daf87cc0527d3053615e
Amarvin <anthony@glacial.com>
2021-03-21 15:33:53 -0400

    Implement basic counters, add interval

--- src/js/bundle.js
+++ src/js/bundle.js
@@ -6,16 +6,54 @@ const load = () => {
 		clicks: 0,
 		x: 0,
 		y: 0,
+		z: 0,
 	};
+	const infoUpdate = () => {
+		for (const divId in numbers) {
+			let div = divId;
+
+			div = document.getElementById(div);
+			if (div) {
+				div.innerHTML = `${divId} = ${numbers[divId]}`;
+			}
+		}
+	};
+
 	clickerButton.addEventListener('click', () => {
-		numbers.x += 1 + numbers.y;
 		numbers.clicks += 1;
+		numbers.x = round(
+			numbers.clicks + 0.01 * round(numbers.y * (0.01 * numbers.z))
+		);
 		if (numbers.clicks % 10 == 0) {
 			numbers.y += 1;
 			console.log(`number Y is: ${numbers.y}`);
 		}
+		if (numbers.y % 10 == 0 && numbers.clicks % 10 == 0) {
+			numbers.z += 1;
+			console.log(`number Z is: ${numbers.z}`);
+		}
 		console.log(numbers.x);
+		infoUpdate();
 		// alert('it work!');
 	});
+	setInterval(() => {
+		numbers.clicks++;
+		numbers.x = round(
+			numbers.clicks + 0.01 * round(numbers.y * (0.01 * numbers.z))
+		);
+		if (numbers.clicks % 10 == 0) {
+			numbers.y += 1;
+			console.log(`number Y is: ${numbers.y}`);
+		}
+		if (numbers.y % 10 == 0 && numbers.clicks % 10 == 0) {
+			numbers.z += 1;
+			console.log(`number Z is: ${numbers.z}`);
+		}
+		infoUpdate();
+	}, 1000);
+
+	function round(value, decimals = 2) {
+		return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals);
+	}
 };
 load();
